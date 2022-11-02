ee452ef20c635a08d77be60a405c8a8e21011e70
Alberto <albertovinicius3@gmail.com>
2022-11-02 15:47:26 -0300

    chore: change colors to override theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,18 +5,18 @@ module.exports = {
         fontFamily: {
             sans: ['Titillium Web', 'sans-serif'],
         },
-        extend: {
-            colors: {
-                red: {
-                    500: '#da0941',
-                    600: '#b80c3a',
-                    800: '#a10831d0',
-                },
-                slate: {
-                    600: '#041a27',
-                    700: '#041722',
-                },
+        colors: {
+            red: {
+                500: '#da0941',
+                600: '#b80c3a',
+                800: '#a10831d0',
             },
+            slate: {
+                600: '#041a27',
+                700: '#041722',
+            },
+        },
+        extend: {
             backgroundImage: {
                 'red-gradient': 'linear-gradient(70deg, #611d2c, #da0941)',
                 'slate-gradient': 'linear-gradient(240deg, #041722, #041a27)',
