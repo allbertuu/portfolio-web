61ab0ea44fc6a64749f21e56b1a07f1a5b10ba42
Alberto <albertovinicius3@gmail.com>
2022-11-08 16:05:45 -0300

    refactor: apply clean code and fix styles import

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './styles/index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+
+root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
