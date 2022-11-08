37014ef379b7ec5f985e745643047105c3e11d09
Alberto <albertovinicius3@gmail.com>
2022-11-08 16:04:47 -0300

    chore: create aboutMe script for myself infos

---
+++ src/assets/scripts/aboutMe.ts
@@ -0,0 +1,5 @@
+export const sociais = {
+    linkedin: 'https://www.linkedin.com/in/albertov-albuquerque',
+    github: 'https://github.com/allbertuu',
+    instagram: 'https://www.instagram.com/albert.vny/',
+};
