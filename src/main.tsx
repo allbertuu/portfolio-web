3dc7d1cfbe78b344cc0bdaee02aacb134034100d
Alberto <albertovinicius3@gmail.com>
2022-11-11 10:30:22 -0300

    feat: add styles to scrollbar and improve responsiveness

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './styles/index.css';
+import './styles/index.scss';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
