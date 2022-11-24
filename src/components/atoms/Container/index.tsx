ecfa560d8d5d0483578ca0ffedc4f2f6de51d876
Alberto <albertovinicius3@gmail.com>
2022-11-24 11:42:35 -0300

    feat(components): add Container atom

---
+++ src/components/atoms/Container/index.tsx
@@ -0,0 +1,23 @@
+import { classNames } from '@utils/index';
+import { FunctionComponent, HTMLAttributes } from 'react';
+
+interface ContainerProps extends HTMLAttributes<HTMLHeadingElement> {}
+
+const Container: FunctionComponent<ContainerProps> = ({
+    children,
+    ...props
+}) => {
+    return (
+        <h3
+            {...props}
+            className={classNames(
+                props.className ? props.className : '',
+                'container mx-auto px-8',
+            )}
+        >
+            {children}
+        </h3>
+    );
+};
+
+export default Container;
