8842ae19c8cbefacbe5f06ea25409d07b0c1610c
Alberto <albertovinicius3@gmail.com>
2022-12-04 13:42:25 -0300

    fix: change index.scss to global.scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './styles/index.scss';
+import './styles/global.scss';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
