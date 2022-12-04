d30cce67741d2eaadb243bc661a239954fe03147
Alberto <albertovinicius3@gmail.com>
2022-12-04 16:41:35 -0300

    test: setup tests environment with testing lib

---
+++ tests/setup.js
@@ -0,0 +1,7 @@
+import '@testing-library/jest-dom';
+
+import { expect } from 'vitest';
+import matchers from '@testing-library/jest-dom/matchers';
+
+// extends Vitest's expect method with methods from react-testing-library
+expect.extend(matchers);
