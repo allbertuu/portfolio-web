bd4649e8523fca80e2abb91c39502684e158eef7
Alberto <albertovinicius3@gmail.com>
2022-12-11 22:18:05 -0300

    chore: change colors theme overide to extend

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,19 +5,20 @@ module.exports = {
         fontFamily: {
             sans: ['Titillium Web', 'sans-serif'],
         },
-        colors: {
-            red: {
-                500: '#da0941',
-                600: '#b80c3a',
-                800: '#a10831d0',
-            },
-            slate: {
-                600: '#041a27',
-                700: '#041722',
-            },
-            white: '#FFF',
-        },
         extend: {
+            colors: {
+                red: {
+                    500: '#da0941',
+                    600: '#b80c3a',
+                    800: '#a10831d0',
+                },
+                slate: {
+                    600: '#041a27',
+                    700: '#041722',
+                    800: '#04131c',
+                },
+                white: '#FFF',
+            },
             backgroundImage: {
                 'red-gradient': 'linear-gradient(70deg, #611d2c, #da0941)',
                 'slate-gradient': 'linear-gradient(240deg, #041722, #041a27)',
