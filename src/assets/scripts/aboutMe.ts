d506738fdfb9a951e39e876e809d9cd607acece6
Alberto <albertovinicius3@gmail.com>
2022-12-11 22:20:36 -0300

    feat(scripts): add instagram user

--- src/assets/scripts/aboutMe.ts
+++ src/assets/scripts/aboutMe.ts
@@ -1,5 +1,8 @@
 export const sociais = {
     linkedin: 'https://www.linkedin.com/in/albertov-albuquerque',
     github: 'https://github.com/allbertuu',
-    instagram: 'https://www.instagram.com/albert.vny/',
+    instagram: {
+        url: 'https://www.instagram.com/albert.vny/',
+        user: '@albert.vny',
+    },
 };
