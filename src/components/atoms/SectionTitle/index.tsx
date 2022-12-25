5e72557af1834c4bc34cca1595c3e54042ed3e86
Alberto <albertovinicius3@gmail.com>
2022-12-25 18:26:50 -0300

    feat: add text prop and organize classes

--- src/components/atoms/SectionTitle/index.tsx
+++ src/components/atoms/SectionTitle/index.tsx
@@ -1,22 +1,23 @@
 import { classNames } from '@utils/index';
-import { FunctionComponent, HTMLAttributes } from 'react';
+import { FunctionComponent } from 'react';
+import { ISectionTitleProps } from './types';
 
-interface SectionTitleProps extends HTMLAttributes<HTMLHeadingElement> {}
-
-const SectionTitle: FunctionComponent<SectionTitleProps> = ({
-    children,
+const SectionTitle: FunctionComponent<ISectionTitleProps> = ({
+    text,
     ...props
 }) => {
     return (
-        <h3
+        <h1
             {...props}
             className={classNames(
                 props.className || '',
-                'text-2xl lg:text-4xl',
+                'relative w-fit',
+                'text-2xl lg:text-4xl text-white font-bold',
+                'after:block after:absolute after:h-[2px] after:w-2/5 after:right-0 after:bg-red-500',
             )}
         >
-            {children}
-        </h3>
+            {text}
+        </h1>
     );
 };
 
