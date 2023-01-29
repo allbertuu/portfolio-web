60254ab6a6e8374960d9df11ee36337aeb83d0cb
Alberto <albertovinicius3@gmail.com>
2023-01-29 15:47:17 -0300

    docs: add JSDocs to classNames

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,9 @@
+/**
+ * Utilitário para criação de classNames condicionais, usando frameworks como TailwindCSS.
+ *
+ * Exemplo: secondary ? 'bg-red-500' : 'bg-blue-500'
+ * @returns Um array de string concatenadas.
+ */
 export const classNames = (...classes: string[]) =>
     classes.filter(Boolean).join(' ');
 
