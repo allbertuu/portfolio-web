544172748865b406b94f11d54123b008a2d494c7
Alberto <albertovinicius3@gmail.com>
2023-01-29 16:33:57 -0300

    docs(container): add JSDocs

--- src/components/layout/Container/index.tsx
+++ src/components/layout/Container/index.tsx
@@ -1,7 +1,10 @@
 import { classNames } from '@utils/index';
 import { FunctionComponent } from 'react';
 import { IContainerProps } from './types';
-
+/**
+ * Alinha o conteúdo do container ao centro, com uma largura máxima que depende
+ * da largura do dispositivo, e adiciona um padding lateral de 2rem (32px)
+ */
 const Container: FunctionComponent<IContainerProps> = ({
     children,
     ...props
