706636f843ccf913629b2838ea0d381786570ade
Alberto <albertovinicius3@gmail.com>
2023-03-20 16:02:18 -0300

    ♻️ refactor(components/index.ts): remove Header and Footer components and add SocialButton component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 export { default as ProjectCard } from './ProjectCard';
 export { default as MySocials } from './MySocials';
-export { default as Header } from './layout/Header';
-export { default as Footer } from './layout/Footer';
+export { default as SocialButton } from './SocialButton';
