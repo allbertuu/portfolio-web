f2596a9775bf0aae4643fc50c018fa51ce24e2c5
Alberto <albertovinicius3@gmail.com>
2023-06-30 12:59:54 -0300

    feat: add new projects

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -14,15 +14,15 @@ const ProjectCard: FunctionComponent<IProjectCardProps> = ({
     return (
         <article
             className={classNames(
-                'relative rounded max-w-xs border-2 border-slate-50/10 p-4 bg-slate-700',
+                'flex flex-col relative rounded max-w-xs border-2 border-slate-50/10 p-4 bg-slate-700',
                 'transition-all duration-300 ease-out',
                 'hover:border-red-500 hover:shadow-xl hover:shadow-red-500/30 hover:transform hover:-translate-y-3',
             )}
         >
             <h4 className="text-red-500 -mb-2 font-semibold">{name}</h4>
             <span className="text-muted text-[85%] italic">{segment}</span>
-            <p className="mt-1 leading-snug">{about}</p>
-            <div className="flex justify-between mt-4">
+            <p className="mt-1 leading-snug flex-1">{about}</p>
+            <div className="flex justify-between">
                 <a
                     href={githubLink}
                     target="_blank"
