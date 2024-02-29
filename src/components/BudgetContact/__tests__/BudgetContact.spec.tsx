a71954fdef66e0b241f85eefa17ac18a90d537a2
Alberto <albertovinicius3@gmail.com>
2024-02-29 12:37:28 -0300

    refactor(budgetcontact): improve functionality of budget contact by email

---
+++ src/components/BudgetContact/__tests__/BudgetContact.spec.tsx
@@ -0,0 +1,12 @@
+import budgetEmail from '@/data/budgetEmail.json';
+import { render, screen } from '@/utils/testUtils';
+import { BudgetContact } from '..';
+
+test('button have the budget email link to reach me out', async () => {
+  render(<BudgetContact />);
+
+  const button = screen.getByText(/obtenha um orçamento agora/i);
+  const emailLink = `mailto:${budgetEmail.recipient}?subject=${budgetEmail.subject}&body=${budgetEmail.body}`;
+
+  expect(button).toHaveAttribute('href', emailLink);
+});
