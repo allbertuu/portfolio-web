82759f2534e83651b3aa354a930f1d1e4fa412b5
Alberto <albertovinicius3@gmail.com>
2024-04-30 14:19:35 -0300

    refactor(projectcard): add default params and validate urls links of github and live demo

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,22 +1,22 @@
 import GitHubIcon from '@/assets/icons/GitHub';
-import { classNames } from '@/utils';
+import { classNames, isWebsiteURLValid } from '@/utils';
 import { Broadcast as BroadcastIcon } from '@phosphor-icons/react';
 import { ExternalLink } from '../ExternalLink';
 
 interface IProjectCardProps extends React.HTMLAttributes<HTMLElement> {
-  name: string;
-  myRole: string;
-  about: string;
-  githubLink: string;
+  name?: string;
+  myRole?: string;
+  about?: string;
+  githubLink?: string;
   liveUrl?: string;
 }
 
 export const ProjectCard: React.FC<IProjectCardProps> = ({
-  name,
-  myRole,
-  about,
-  githubLink,
-  liveUrl,
+  name = 'Projeto sem nome',
+  myRole = 'Sem cargo',
+  about = 'Me consulte para mais informações.',
+  githubLink = '',
+  liveUrl = '',
   ...props
 }) => {
   return (
@@ -31,7 +31,7 @@ export const ProjectCard: React.FC<IProjectCardProps> = ({
       <p className="my-2 leading-snug flex-1 text-[90%]">{about}</p>
 
       <div className="flex justify-between">
-        {githubLink && (
+        {isWebsiteURLValid(githubLink) && (
           <ExternalLink
             href={githubLink}
             className="flex gap-1 items-center w-fit hover:opacity-70 transition-all font-normal"
@@ -40,7 +40,7 @@ export const ProjectCard: React.FC<IProjectCardProps> = ({
           </ExternalLink>
         )}
 
-        {liveUrl && (
+        {isWebsiteURLValid(liveUrl) && (
           <ExternalLink
             href={liveUrl}
             className="flex gap-1 items-center w-fit hover:opacity-70 transition-all font-normal"
