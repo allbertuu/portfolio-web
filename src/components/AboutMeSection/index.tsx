ffe9b2e7a2dc75d59237408c2809ea80a7c0ea38
Alberto <albertovinicius3@gmail.com>
2024-09-23 20:58:20 -0300

    🦄 refactor(about-me-section): make texts more concise and reorder mini-sections
    resolve #43

--- src/components/AboutMeSection/index.tsx
+++ src/components/AboutMeSection/index.tsx
@@ -9,7 +9,6 @@ import {
   GlobeHemisphereWest,
   Heart,
   Heart as HeartIcon,
-  Toolbox,
   UserList as UserListIcon,
   Wrench,
 } from '@phosphor-icons/react';
@@ -25,6 +24,7 @@ export const AboutMeSection = () => {
   const techLinks = {
     react: 'https://react.dev/',
     javascript: 'https://developer.mozilla.org/pt-BR/docs/Web/JavaScript',
+    typescript: 'https://www.typescriptlang.org/',
   };
 
   return (
@@ -38,10 +38,53 @@ export const AboutMeSection = () => {
           className="w-full sm:w-3/4 mx-auto pb-6 delaySmallReveal"
           alt="Ilustração de uma pessoa sentada em frente a uma fogueira em um lugar com árvores e a luz da lua ao fundo"
         />
+
         <h2 className="section__title text-primary mb-4 originLeftReveal intervalReveal">
           Sobre mim
         </h2>
+
         <div role={'group'} className="mt-2 delayMediumReveal text-black/90">
+          <article role="listitem" className="intervalReveal">
+            <div className="flex gap-2 items-center text-accent mb-2">
+              <BriefcaseIcon className="text-2xl" weight="fill" />
+              <h4 className="leading-none">Minha profissão</h4>
+            </div>
+
+            <p>
+              Sou Desenvolvedor de Sistemas, com{' '}
+              <ClockCountdown
+                className="text-accent -mt-1 inline-block"
+                weight="fill"
+              />{' '}
+              {formatDistanceToNow(profession.startDate, {
+                locale: ptBR,
+              })}{' '}
+              de experiência em{' '}
+              <abbr title="Responsável por criar interfaces de sistemas que sejam agradáveis, úteis e fáceis de usar.">
+                <em>Front-End</em>
+              </abbr>
+              , capaz de resolver problemas simplificando-os de forma muito
+              prática e eficiente usando habilidades técnicas avançadas que
+              ajudaram as empresas que passei a ter resultados desde performance
+              de aplicações à aperfeiçoamento de requisitos de sistema, e tenho
+              profundo conhecimento em{' '}
+              <ExternalLink href={techLinks.javascript}>
+                <em>React</em>
+              </ExternalLink>{' '}
+              ,{' '}
+              <ExternalLink href={techLinks.typescript}>
+                <em>TypeScript</em>
+              </ExternalLink>{' '}
+              ,{' '}
+              <ExternalLink href={techLinks.javascript}>
+                <em>JavaScript</em>
+              </ExternalLink>
+              , <em>HTML</em> e <em>CSS</em>.
+            </p>
+          </article>
+
+          <div className="separator my-8 intervalReveal" />
+
           <article role="listitem" className="intervalReveal">
             <div className="flex gap-2 items-center text-accent mb-2">
               <UserListIcon className="text-3xl" weight="fill" />
@@ -55,22 +98,15 @@ export const AboutMeSection = () => {
             </p>
 
             <p>
-              Sou{' '}
-              <strong>reservado, autodisciplinado, detalhista e curioso</strong>
-              . Procuro ser alguém que está de fato, vivendo a vida, com sonhos,
-              responsabilidades e amigos fieis.
+              Posso me descrever como reservado, autodisciplinado, detalhista e
+              curioso.
             </p>
 
             <p>
               Atualmente tenho {myCurrentAge} anos, e estou cursando{' '}
               <strong>{education.course}</strong>{' '}
-              <Cpu className="text-accent inline-block" weight="regular" /> na{' '}
-              {education.institution}{' '}
-              {formatDistanceToNow(education.startDate, {
-                addSuffix: true,
-                locale: ptBR,
-              })}
-              .
+              <Cpu className="text-accent inline-block" weight="fill" /> na{' '}
+              {education.institution}.
             </p>
           </article>
 
@@ -86,17 +122,14 @@ export const AboutMeSection = () => {
               Quero poder trabalhar remoto, viajando pelo mundo{' '}
               <GlobeHemisphereWest
                 className="text-accent inline-block"
-                weight="regular"
+                weight="fill"
               />{' '}
-              e vivendo novas experiências junto a minha esposa. <br />A
-              tecnologia <strong>me possibilita</strong> esse sonho.
+              e vivendo novas experiências.{' '}
+              <strong>A tecnologia me possibilita esse sonho</strong>.
             </p>
 
             <p>
-              <HeartIcon
-                className="text-accent inline-block"
-                weight="regular"
-              />{' '}
+              <HeartIcon className="text-accent inline-block" weight="fill" />{' '}
               Me interesso principalmente por <em>web design</em>, construção de
               interfaces centradas no usuário (<em>UI/UX Design</em>), e{' '}
               <em>
@@ -106,8 +139,8 @@ export const AboutMeSection = () => {
               </em>
               . Gosto e tenho aptidão em criar soluções, resolver problemas
               reais decompondo-os{' '}
-              <Wrench className="text-accent inline-block" weight="regular" />{' '}
-              em partes menores e mais simples, pouco importa a tecnologia usada
+              <Wrench className="text-accent inline-block" weight="fill" /> em
+              partes menores e mais simples, pouco importa a tecnologia usada
               para isso.
             </p>
 
@@ -115,62 +148,11 @@ export const AboutMeSection = () => {
               Amo ler livros{' '}
               <BookBookmark
                 className="text-accent inline-block"
-                weight="regular"
+                weight="fill"
               />{' '}
               e aproveitar uma ótima história. Gosto de entender o ser humano,
               então sempre estou pesquisando sobre psicologia, filosofia e temas
-              relacionados à estes dos quais me fascinam.
-            </p>
-          </article>
-
-          <div className="separator my-8 intervalReveal" />
-
-          <article role="listitem" className="intervalReveal">
-            <div className="flex gap-2 items-center text-accent mb-2">
-              <BriefcaseIcon className="text-2xl" weight="fill" />
-              <h4 className="leading-none">Minha profissão</h4>
-            </div>
-
-            <p>
-              Com{' '}
-              <strong>
-                {formatDistanceToNow(profession.startDate, {
-                  locale: ptBR,
-                })}
-              </strong>{' '}
-              de experiência em <strong>desenvolvimento de software</strong>,
-              comecei minha carreira como{' '}
-              <abbr title="Sou capaz de criar interfaces de sistemas que sejam agradáveis, úteis e fáceis de usar.">
-                <em>Frontend Developer</em>
-              </abbr>
-              , desenvolvendo interfaces como portais de vagas, sistemas de
-              contabilidade, landing pages e criação de software personalizado.
-              Já atuei tanto para startups, quanto empresas de pequeno porte.
-            </p>
-
-            <p>
-              <ClockCountdown
-                className="text-accent -mt-1 inline-block"
-                weight="regular"
-              />{' '}
-              Hoje em dia desenvolvo soluções para a web, como aplicações de
-              ponta a ponta <em>backend</em> + <em>frontend</em>, estáveis e
-              performáticas. Sempre com um olhar apurado para o design e
-              usabilidade.
-            </p>
-
-            <p>
-              Minha stack de ferramentas{' '}
-              <Toolbox className="text-accent inline-block" weight="regular" />{' '}
-              principais são{' '}
-              <ExternalLink href={techLinks.javascript}>
-                <em>JavaScript</em>
-              </ExternalLink>{' '}
-              e{' '}
-              <ExternalLink href={techLinks.react}>
-                <em>React.js</em>
-              </ExternalLink>
-              .
+              relacionados.
             </p>
           </article>
         </div>
