dc5b836410883dcfc94b86499e68af15de30c32b
Alberto <albertovinicius3@gmail.com>
2025-12-04 10:28:54 -0300

    🦄 refactor(projects): remove unnecessary data and comps

--- src/components/SideProjectsSection/index.tsx
+++ src/components/SideProjectsSection/index.tsx
@@ -1,7 +1,4 @@
 import GitHubIcon from '@/assets/icons/GitHub';
-// import profession from '@/data/profession.json';
-// import { ExternalLink } from '../ExternalLink';
-// import { ProjectCard } from '../ProjectCard';
 
 export const SideProjectsSection = () => {
   return (
@@ -34,31 +31,6 @@ export const SideProjectsSection = () => {
           <GitHubIcon fill="#000" />
           Perfil no GitHub
         </a>
-
-        {/* <div
-          role="group"
-          className="flex flex-wrap justify-center gap-12 mt-8 mb-6 originBottomReveal"
-        >
-          {profession.projects.map((project) => (
-            <ProjectCard
-              key={project.name}
-              name={project.name}
-              stack={project.stack}
-              githubLink={project.links.github}
-              liveUrl={project.links.site}
-              about={project.about}
-              role="listitem"
-            />
-          ))}
-        </div> */}
-
-        {/* <p className="text-center text-lg flex gap-1 items-center justify-center originBottomReveal">
-          <ExternalLink href="https://github.com/allbertuu?tab=repositories&q=&type=&language=&sort=stargazers">
-            Clique aqui e confira (muito) mais
-          </ExternalLink>
-
-          <GitHubIcon fill="rgb(226 232 240 / 0.95)" />
-        </p> */}
       </div>
     </section>
   );
