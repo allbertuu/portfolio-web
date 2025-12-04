08c453bf9ab7c7d77d6fcef854d57bb2a8b89fe6
Alberto <albertovinicius3@gmail.com>
2025-12-04 10:25:17 -0300

    ✨ feat(projects): add github link to projects list
    In the Projects section, I will simply include a link to my GitHub, where my best pinned projects will be on the first page.  I don't see the need to create and manage this redundancy. An integrated GitHub list would require error handling, maintenance, and other factors.  All my projects include a good readme and complete information, suitable for developers or beginners.
    
    resolve #44

--- src/components/SideProjectsSection/index.tsx
+++ src/components/SideProjectsSection/index.tsx
@@ -1,17 +1,41 @@
 import GitHubIcon from '@/assets/icons/GitHub';
-import profession from '@/data/profession.json';
-import { ExternalLink } from '../ExternalLink';
-import { ProjectCard } from '../ProjectCard';
+// import profession from '@/data/profession.json';
+// import { ExternalLink } from '../ExternalLink';
+// import { ProjectCard } from '../ProjectCard';
 
 export const SideProjectsSection = () => {
   return (
-    <section id="projects" className="py-16 mt-6 border-b-2 border-b-accent">
-      <div className="container mx-auto px-4">
+    <section
+      id="projects"
+      className="py-10 pb-14 mt-6 border-b-2 border-b-accent"
+    >
+      <div className="container mx-auto px-4 max-w-2xl">
         <h2 className="section__title mx-auto originLeftReveal">
           Meus projetos
         </h2>
 
-        <div
+        <p className="mt-4 text-center text-lg originBottomReveal">
+          Abaixo você encontra facilmente os{' '}
+          <strong>meus melhores projetos de software</strong> aberto (
+          <i>open-source</i>).
+        </p>
+
+        <p className="mb-4 text-center text-lg originBottomReveal">
+          Todos com ótima apresentação 🎀, documentação 📘 e tudo que você
+          precisa saber para entender o projeto <strong>rápido</strong>!
+        </p>
+
+        <a
+          className="mx-auto no-underline mt-6 flex w-fit items-center gap-2 rounded-md bg-white px-4 py-2 text-lg font-bold text-black  originBottomReveal"
+          href="http://github.com/allbertuu"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHubIcon fill="#000" />
+          Perfil no GitHub
+        </a>
+
+        {/* <div
           role="group"
           className="flex flex-wrap justify-center gap-12 mt-8 mb-6 originBottomReveal"
         >
@@ -26,15 +50,15 @@ export const SideProjectsSection = () => {
               role="listitem"
             />
           ))}
-        </div>
+        </div> */}
 
-        <p className="text-center text-lg flex gap-1 items-center justify-center originBottomReveal">
+        {/* <p className="text-center text-lg flex gap-1 items-center justify-center originBottomReveal">
           <ExternalLink href="https://github.com/allbertuu?tab=repositories&q=&type=&language=&sort=stargazers">
             Clique aqui e confira (muito) mais
           </ExternalLink>
 
           <GitHubIcon fill="rgb(226 232 240 / 0.95)" />
-        </p>
+        </p> */}
       </div>
     </section>
   );
